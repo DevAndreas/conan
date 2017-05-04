8167103bf4fae951873ed8b785df4b0655146b18
DevAndreas <achernov@byterox.com>
2017-05-05 01:03:21 +0700

    add connan

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <string>
 #include "hello.h"
+#include <boost/lambda/lambda.hpp>
+#include <vector>
+#include <algorithm>
+
+using namespace boost::lambda;
 
 int main()
 {
-    std::string s("Hello, World");
+    std::vector<int> figures = {1,2,3,4,5};
+    std::for_each(std::begin(figures), std::end(figures), std::cout << (_1 * 3) << " " );
+
+    std::string s("Hello, World!!");
     std::cout << hello(s) << std::endl;
     return(0);
 }
