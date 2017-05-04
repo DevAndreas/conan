8e79be564d82818c8b29aa50c9b8de8dc6fd88a7
DevAndreas <achernov@byterox.com>
2017-05-05 02:53:02 +0700

    bbb

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ using namespace boost::lambda;
 
 int main()
 {
-    std::vector<int> figures = {1,2,3,4,5};
-    std::for_each(std::begin(figures), std::end(figures), std::cout << (_1 * 3) << " " );
+//    std::vector<int> figures = {1,2,3,4,5};
+//    std::for_each(std::begin(figures), std::end(figures), std::cout << (_1 * 3) << " " );
 
     std::string s("Hello, World!!");
     std::cout << hello(s) << std::endl;
